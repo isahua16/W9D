6cb46210726356f414ef7aa41fd485502d9dd9a6
isahua16 <isaelhuard@gmail.com>
2023-03-18 15:21:49 -0600

    created the array of tweets

---
+++ js/app.js
@@ -0,0 +1,63 @@
+let tweets = [
+    {
+        content: `Just tried my first avocado toast and I'm officially obsessed! 😍 #avocadotoast #brunchlife`,
+        user: `@foodie4life`,
+        date: `3/18/23`,
+        user_age: 27
+    },
+    {
+        content: `Can't believe I got to meet my favorite author today! 😱 Such an inspiring experience. #bookworm #authorlove`,
+        user: `@bibliophile21`,
+        date: `3/17/23`,
+        user_age: 32
+    },
+    {
+        content: `Just finished a 10-mile run and feeling invincible! 💪 #runnershigh #fitlife`,
+        user: `@marathonmaniac`,
+        date: `3/1/23`,
+        user_age: 40
+    },
+    {
+        content: `Finally got my driver's license! 🚗 Look out world, here I come! #newdriver #roadtripper`,
+        user: `@adventurer27`,
+        date: `3/2/23`,
+        user_age: 16
+    },
+    {
+        content: `Loving this new coffee shop I found! ☕️ The decor is so cozy and the latte is amazing. #coffeelover #localgems`,
+        user: `@caffeineaddict`,
+        date: `3/4/23`,
+        user_age: 22
+    },
+    {
+        content: `Just got my acceptance letter to my dream university! 🎓 Hard work really does pay off. #collegelife #goals`,
+        user: `@determined2023`,
+        date: `3/7/23`,
+        user_age: 18
+    },
+    {
+        content: `Watching the sunset over the ocean is pure magic. 🌅 So grateful for this moment. #oceanlover #grateful`,
+        user: `@beachbum23`,
+        date: `3/10/23`,
+        user_age: 29
+    },
+    {
+        content: `Tried a new vegan recipe tonight and it was a hit with the whole family! 🌱 #veganlife #healthyeats`,
+        user: `@plantbasedmomma`,
+        date: `3/11/23`,
+        user_age: 35
+    },
+    {
+        content: `Just finished a 12-hour coding session and I feel like a genius. 😎 #programmerlife #codinglove`,
+        user: `@techgeek21`,
+        date: `3/12/23`,
+        user_age: 24
+    },
+    {
+        content: `Can't believe it's been a year since I adopted my furry best friend! 🐾 She brings so much joy to my life. #dogmom #adoptdontshop`,
+        user: `@animallover365`,
+        date: `3/13/23`,
+        user_age: 31
+    }
+];
+
